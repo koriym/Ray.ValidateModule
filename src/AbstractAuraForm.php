aa46cc89e4bd47c14b0ca8ad6c69e0089fe49794
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-08-21 02:11:01 +0900

    add input / error method

--- src/AbstractAuraForm.php
+++ src/AbstractAuraForm.php
@@ -27,9 +27,35 @@ abstract class AbstractAuraForm extends Form
     }
 
     /**
-     * @return string
+     * Return input element html
      *
+     * @param string $input
+     *
+     * @return string
      * @throws \Aura\Input\Exception\NoSuchInput
      */
-    abstract public function __toString();
+    public function input($input)
+    {
+        return $this->helper->input($this->get($input));
+    }
+
+    /**
+     * Return error message
+     *
+     * @param string $input
+     * @param string $format
+     * @param string $layout
+     *
+     * @return string
+     */
+    public function error($input, $format = '%s', $layout = '%s')
+    {
+        $errorMessages = $this->getFilter()->getMessages($input);
+        array_filter($errorMessages, function (&$item) use ($format) {
+            $item = sprintf($format, $item);
+        });
+        $errors = implode('', $errorMessages);
+
+        return  sprintf($layout, $errors);
+    }
 }
